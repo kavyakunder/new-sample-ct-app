05b69e4f2044a49cd4d2051659935a2798cfe159
kavyakunder10 <kavyakunder7082@gmail.com>
2024-11-01 18:46:18 +0530

    add to github

--- free-sample-product-service/src/utils/logger.utils.test.ts
+++
@@ -1,7 +0,0 @@
-import { logger } from "./logger.utils"
-
-describe("logger object", () => {
-    it("should not throw an error when logger object", () => {
-        expect(typeof logger).toEqual('object');
-    });
-});
