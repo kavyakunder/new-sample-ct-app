43f2ccbf89308b36d67202f8bc3eaf997278594e
kavyakunder10 <kavyakunder7082@gmail.com>
2024-11-04 11:04:00 +0530

    remove custom app

--- first-connect-application/merchant-center-custom-app/new-cust-app/src/constants.js
+++
@@ -1,6 +0,0 @@
-// Make sure to import the helper functions from the `ssr` entry point.
-import { entryPointUriPathToPermissionKeys } from '@commercetools-frontend/application-shell/ssr';
-
-export const entryPointUriPath = 'starter-728143';
-
-export const PERMISSIONS = entryPointUriPathToPermissionKeys(entryPointUriPath);
