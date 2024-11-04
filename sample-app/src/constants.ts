66f421434a6a4aa6bf7d34853317c96d618659fc
kavyakunder10 <kavyakunder7082@gmail.com>
2024-11-04 16:08:25 +0530

    deploy

--- sample-app/src/constants.ts
+++ sample-app/src/constants.ts
@@ -1,12 +1,9 @@
 // Make sure to import the helper functions from the `ssr` entry point.
 import { entryPointUriPathToPermissionKeys } from '@commercetools-frontend/application-shell/ssr';
 
-const newEntryPointUriPath = 'my-app-1';
-
-
 export const entryPointUriPath =
     typeof window === 'undefined'
-        ? process.env.ENTRY_POINT_URI_PATH || newEntryPointUriPath
-        : newEntryPointUriPath;
+        ? process.env.ENTRY_POINT_URI_PATH
+        : window.app.entryPointUriPath;
 
 export const PERMISSIONS = entryPointUriPathToPermissionKeys(entryPointUriPath);
