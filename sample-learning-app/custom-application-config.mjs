3051c244f38cf25e3fef8d994e9db780748bc53f
kavyakunder10 <kavyakunder7082@gmail.com>
2024-11-04 17:11:24 +0530

    sample-app

--- sample-learning-app/custom-application-config.mjs
+++ sample-learning-app/custom-application-config.mjs
@@ -4,16 +4,15 @@ import { PERMISSIONS, entryPointUriPath } from './src/constants';
  * @type {import('@commercetools-frontend/application-config').ConfigOptionsForCustomApplication}
  */
 const config = {
-  name: 'Starter Typescript 191be9',
+  name: 'Sample Learning App',
   entryPointUriPath,
-  cloudIdentifier: 'gcp-eu',
+  cloudIdentifier: '${env:CLOUD_IDENTIFIER}',
   env: {
     development: {
-      initialProjectKey: 'sample-learning-project',
+      initialProjectKey: '${env:CTP_PROJECT_KEY}',
     },
     production: {
-      applicationId: 'TODO',
-      url: 'https://your_app_hostname.com',
+      applicationId: '${env:CUSTOM_APPLICATION_ID}',
     },
   },
   oAuthScopes: {
@@ -22,7 +21,7 @@ const config = {
   },
   icon: '${path:@commercetools-frontend/assets/application-icons/rocket.svg}',
   mainMenuLink: {
-    defaultLabel: 'Template starter',
+    defaultLabel: 'Sample Learning App',
     labelAllLocales: [],
     permissions: [PERMISSIONS.View],
   },
