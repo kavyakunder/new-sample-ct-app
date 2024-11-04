03ff2912068f2e7b33472e56ad91b3e260dacaca
kavyakunder10 <kavyakunder7082@gmail.com>
2024-11-04 16:54:38 +0530

    new-files

--- quota-manager/src/index.tsx
+++
@@ -1,7 +0,0 @@
-/// <reference types="@commercetools-frontend/application-config/client" />
-
-import ReactDOM from 'react-dom';
-import EntryPoint from './components/entry-point';
-import './style.css';
-
-ReactDOM.render(<EntryPoint />, document.getElementById('app'));
