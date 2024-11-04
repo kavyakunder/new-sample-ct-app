073303ad2d9cc8646eaae16f49d0cf1327b4f0c3
kavyakunder10 <kavyakunder7082@gmail.com>
2024-11-04 18:20:03 +0530

    new-changes

--- sample-learning-app/src/constants.ts
+++ sample-learning-app/src/constants.ts
@@ -2,8 +2,8 @@
 import { entryPointUriPathToPermissionKeys } from '@commercetools-frontend/application-shell/ssr';
 
 export const entryPointUriPath =
-  typeof window === 'undefined'
-    ? process.env.ENTRY_POINT_URI_PATH
-    : window.app.entryPointUriPath;
+    typeof window === 'undefined'
+        ? process.env.ENTRY_POINT_URI_PATH
+        : window.app.entryPointUriPath;
 
 export const PERMISSIONS = entryPointUriPathToPermissionKeys(entryPointUriPath);
--- sample-learning-app/src/routes.tsx
+++ sample-learning-app/src/routes.tsx
@@ -1,7 +1,9 @@
 import type { ReactNode } from 'react';
 import { Switch, Route, useRouteMatch } from 'react-router-dom';
 import Spacings from '@commercetools-uikit/spacings';
-import QuotaManager from './components/quota-manager';
+import Channels from './components/channels';
+import Welcome from './components/welcome';
+
 type ApplicationRoutesProps = {
   children?: ReactNode;
 };
@@ -12,7 +14,7 @@ const ApplicationRoutes = (_props: ApplicationRoutesProps) => {
    * When using routes, there is a good chance that you might want to
    * restrict the access to a certain route based on the user permissions.
    * You can evaluate user permissions using the `useIsAuthorized` hook.
-   * For more information see https://docs.commercetools.com/custom-applications/development/permissions
+   * For more information see https://docs.commercetools.com/merchant-center-customizations/development/permissions
    *
    * NOTE that by default the Custom Application implicitly checks for a "View" permission,
    * otherwise it won't render. Therefore, checking for "View" permissions here
@@ -22,8 +24,11 @@ const ApplicationRoutes = (_props: ApplicationRoutesProps) => {
   return (
     <Spacings.Inset scale="l">
       <Switch>
-        <Route path={`${match.path}/manager`}>
-          <QuotaManager />
+        <Route path={`${match.path}/channels`}>
+          <Channels linkToWelcome={match.url} />
+        </Route>
+        <Route>
+          <Welcome />
         </Route>
       </Switch>
     </Spacings.Inset>
